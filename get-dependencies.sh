4df59cf1e520b87633cfc15f6405ee397fd56533
Link4Electronics <link4electronics@gmail.com>
2026-05-01 11:17:36 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -16,7 +16,7 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package llvm19
+PRE_BUILD_CMDS="sed -i 's/-DLLVM_BUILD_TESTS=ON/-DLLVM_BUILD_TESTS=OFF/; /check() {/,/}/ s/^/#/' ./PKGBUILD" make-aur-package llvm19
 make-aur-package lld19
 make-aur-package clang19
 make-aur-package duckstation
