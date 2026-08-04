1b1c039a5282817f369bcef0b182959447fea634
Link4Electronics <link4electronics@gmail.com>
2026-08-04 20:31:57 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -20,6 +20,7 @@ PRE_BUILD_CMDS="sed -i 's/-DLLVM_BUILD_TESTS=ON/-DLLVM_BUILD_TESTS=OFF/; /check(
 make-aur-package lld19
 make-aur-package compiler-rt19
 PRE_BUILD_CMDS="sed -i 's/-DLLVM_BUILD_TESTS=ON/-DLLVM_BUILD_TESTS=OFF/; /check() {/,/}/ s/^/#/' ./PKGBUILD" make-aur-package clang19
+make-aur-package scc
 make-aur-package duckstation
 
 # If the application needs to be manually built that has to be done down here
